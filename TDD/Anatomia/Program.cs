6674c724195437e56615eee7911a3f72cfcd6f30
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2019-12-17 10:40:13 +0100

    first test example

---
+++ TDD/Anatomia/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace Anatomia
+{
+    class Program
+    {
+        static void Main()
+        {
+            Sum_should_return_the_sum_of_arguments();
+
+            //System.Reflection.Assembly.GetCallingAssembly()
+            //    .GetTypes()
+            //    .Where(t=>t.IsPublic == true)
+            //    .SelectMany(t=>t.GetMethods())
+            //    .Where(m=>m.IsPublic == true)
+        }
+
+        private static void Assembly()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Sum_should_return_the_sum_of_arguments()
+        {
+
+            // arrange 
+            var a = double.MaxValue;
+            var b = 2;
+
+
+            // act
+            var result = Sum(a, b);
+
+
+            // assert
+            if (result == 3)
+            {
+                Console.WriteLine("Pass");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+        }
+
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ TDD/TDD/XunitExampleTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+
+namespace TDD
+{
+    public class XunitExampleTests : IDisposable
+    {
+        public XunitExampleTests()
+        {
+        }
+                
+        [Fact]
+        public void Unit_test_example()
+        {
+            // arrange
+
+            // act
+
+            // assert
+            Assert.True(true);
+        }
+
+        public void Dispose()
+        {
+        }
+
+    }
+}
