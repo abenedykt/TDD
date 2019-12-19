7ad21ac5029aa17104e5431b05a4e415a673eba4
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2019-12-19 10:37:59 +0100

    added example with fluentassertions and restassured

---
+++ TDD/Integration/RestAssuredTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Newtonsoft.Json.Linq;
+using RA;
+using Xunit;
+
+namespace Integration
+{
+    public class RestAssuredTests
+    {
+        [Fact]
+        public void Testing_api_example_with_restassured()
+        {
+            var json = new RestAssured()
+                .Given()
+                    .Name("fetching posts")
+                .When()
+                    .Get("https://jsonplaceholder.typicode.com/posts")
+                .Then()
+                .Retrieve(x => x) as JArray;
+
+            var result = json.ToObject<IEnumerable<Post>>();
+
+            Assert.Equal(100, result.Count());
+        }
+
+        [Fact]
+        public void Testing_api_example_with_restassured_and_fluent_assertions()
+        {
+            var json = new RestAssured()
+                .Given()
+                    .Name("fetching posts")
+                .When()
+                    .Get("https://jsonplaceholder.typicode.com/posts")
+                .Then()
+                .Retrieve(x => x) as JArray;
+
+            var result = json.ToObject<IEnumerable<Post>>();
+
+            result.Should().HaveCount(100);
+        }
+
+    }
+}
---
+++ TDD/TDD/FizzBuzzGameTestsRefactoredWithFluentAssertions.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace TDD
+{
+    public class FizzBuzzGameRefactoredWithFluentAssertionsTests
+    {
+        [Theory,
+        InlineData(1, "1"),
+        InlineData(2, "2"),
+        InlineData(3, "Fizz"),
+        InlineData(5, "Buzz"),
+        InlineData(15, "Fizz Buzz")]
+        public void Should_return_values_according_to_FizzBuzz_game_rulez(int input, string expected)
+        {
+
+            // arrange
+            var game = new FizzBuzzGame();
+            
+            // act
+            var result = game.Play(input);
+
+            // assert
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void Should_throw_invalid_argument_exception_when_input_is_lower_than_1()
+        {
+            var game = new FizzBuzzGame();
+
+            Assert.Throws<InvalidArgumentException>(()=> game.Play(-1));
+        }
+    }
+}
